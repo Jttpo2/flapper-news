f99f00f162c1b3de2331b0b17ef25f617145d2fd
John Petersson <johnpetersson@gmail.com>
2015-03-13 21:57:31 +0100

    database working

--- models/Comments.js
+++ models/Comments.js
@@ -1,12 +1,10 @@
 var mongoose = require('mongoose');
 
-var CommentSchema = new mongooseSchema({
-    body: String,
-    author: String,
-    upvotes: {type: Number, default: 0},
-    posts: { type: mongoose.Schema.Types.ObjectId, ref: 'Post'}
-
-
+var CommentSchema = new mongoose.Schema({
+  body: String,
+  author: String,
+  upvotes: {type: Number, default: 0},
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
 });
 
 mongoose.model('Comment', CommentSchema);
